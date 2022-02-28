08b94ee30ad1a870828da7077044f0c8c26f0607
Austin Pratt <hargis32@gmail.com>
2022-02-27 21:20:55 -0600

    setup variables to match fields from html

---
+++ script.js
@@ -0,0 +1,10 @@
+const APIkey = "ebcf5abc32da5ce2874d3f6d78690cb8";
+const citySearchEl = document.getElementById('city-search');
+const cityNameEl = document.getElementById('city-name');
+const searchHistory = document.querySelector('#search-history');
+const todayForecast = document.getElementById('forecast-today');
+const todayForecastCont = document.getElementById('forecast-today-container');
+const fiveDayForecast = document.getElementById('forecast-five-day');
+const fiveDayForecastCont = document.getElementById('forecast-five-day-container');
+const previousCities = [];
+
